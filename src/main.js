8dfac23b80d885dfc8ddb3719a1a9c10e8a2297d
Igor Caires <macbookpro@Igor-Caires.local>
2019-10-15 14:42:05 -0300

    Initiating Vue Project

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import 'font-awesome/css/font-awesome.css';
+
+import App from './App'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
