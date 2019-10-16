8058d56b6c30cb7c9c70865535db68be4857dfaf
Igor Caires <macbookpro@Igor-Caires.local>
2019-10-15 21:31:04 -0300

    Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<div id="app">
+	<div id="app" :class="{'hide-menu': !isMenuVisible}">
 		<Header title="Cod3r - Base de Conhecimento" :hideToogle="false" />
 		<Menu />
 		<Content />
@@ -8,6 +8,8 @@
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 import Header from './components/template/Header';
 import Menu from './components/template/Menu';
 import Content from './components/template/Content';
@@ -18,6 +20,7 @@ export default {
 	components: {
 		Header, Menu, Content, Footer,
 	},
+	computed: mapState(['isMenuVisible']),
 }
 </script>
 
@@ -42,5 +45,12 @@ export default {
 			"header header"
 			"menu content"
 			"menu footer"
+	}
+
+	#app.hide-menu {
+		grid-template-areas: 
+			"header header"
+			"content content"
+			"footer footer"
 	}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import 'font-awesome/css/font-awesome.css';
 
 import App from './App'
 
+import store from './config/store';
+
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app')
