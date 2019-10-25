63488d046b020af6641ad2c61e880a85317c607d
Igor Caires <macbookpro@Igor-Caires.local>
2019-10-25 10:12:36 -0300

    Cad User

--- src/components/admin/AdminPages.vue
+++ src/components/admin/AdminPages.vue
@@ -1,15 +1,39 @@
 <template>
   <div class="admin-pages">
     <PageTitle icon="fa fa-cogs" main="Administracao do Sistema" sub="Cadastros" />
+    <div class="admin-pages-tabs">
+      <b-card no-body>
+        <b-tabs card>
+          <b-tab title="Artigos" active>
+            <ArticleAdmin />
+          </b-tab>
+          <b-tab title="Categorias" active>
+            <CategoryAdmin />
+          </b-tab>
+          <b-tab title="Usuarios" active>
+            <UserAdmin />
+          </b-tab>
+        </b-tabs>
+      </b-card>
+    </div>
   </div>
 </template>
 
 <script>
 import PageTitle from '../template/PageTitle';
 
+import ArticleAdmin from './ArticleAdmin';
+import CategoryAdmin from './CategoryAdmin';
+import UserAdmin from './UserAdmin';
+
 export default {
   name: 'AdminPages',
-  components: { PageTitle },
+  components: { 
+    PageTitle,
+    ArticleAdmin,
+    CategoryAdmin,
+    UserAdmin,
+   },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -8,10 +8,13 @@ import './config/bootstrap';
 import store from './config/store';
 import router from './config/router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+//TEMPORARIO
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Iklnb3IgUGVkcm8gQ2FpcmVzIiwiZW1haWwiOiJpcGVkcm9jYWlyZXNAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU3MjAwNTMwMywiZXhwIjoxNTcyMjY0NTAzfQ.XxLrr35knk_3wikFsS8TZJWm1xSuWYrw89qKRObUqPM';
 
 new Vue({
   store,
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
